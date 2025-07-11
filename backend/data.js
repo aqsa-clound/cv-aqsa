3dedea582a9d6b7bed91574ca63546fb613262e0
aqsa-clound <muhaidilaqsa@gmail.com>
2025-07-11 22:45:03 +0700

    feat: prepare for initial deployment

---
+++ backend/data.js
@@ -0,0 +1,19 @@
+// backend/data.js
+const educationHistory = [
+  { period: "2019-2023", institution: "MAN 1 KENDARI" },
+  {
+    period: "2023-Sekarang",
+    institution: "UNIVERSITAS AMIKOM YOGYAKARTA",
+    major: "S1 INFORMATIKA",
+  },
+];
+const skills = [{ name: "Geme dev", level: "Beginner" }];
+const projects = [
+  {
+    title: "Profile Web",
+    image:
+      "https://tse1.mm.bing.net/th/id/OIP.CgPULHqW1I9YAn0QBzJoNQHaFj?pid=Api&P=0&h=220",
+    description: "askjdhadkjadhakldaodha aiodhadhi9oaodhiahod aoidhoiadoihdhoa",
+  },
+];
+module.exports = { educationHistory, skills, projects };
